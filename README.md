fe5088518361ed56567b9133822ecbeaf823980f
dastagg:opus5 <dastagg@stagglinux.com>
2022-02-15 21:36:08 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# xylophone
+
+A Flutter project.
+
+## A London App Brewery Flutter Bootcamp tutorial application.
+
+This is code typed in or copy/pasted in the process of learning to create applications using Flutter.
+
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(const XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  static AudioCache player = AudioCache();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                player.play('note1.wav');
+              },
+              child: const Text('Click Me'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
