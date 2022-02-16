4896f51c333c2a9e97527075a25ca4baa25c6621
dastagg:opus5 <dastagg@stagglinux.com>
2022-02-15 22:16:09 -0500

    Section 9.4 Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,53 @@ class XylophoneApp extends StatelessWidget {
 
   static AudioCache player = AudioCache();
 
+  void playSound(int noteNumber) {
+    player.play('note$noteNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                player.play('note1.wav');
-              },
-              child: const Text('Click Me'),
-            ),
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {playSound(1); },
+                child: const Text(''),
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {playSound(2); },
+                child: const Text(''),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {playSound(3); },
+                child: const Text(''),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {playSound(4); },
+                child: const Text(''),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {playSound(5); },
+                child: const Text(''),
+              ),
+              FlatButton(
+                color: Colors.indigo,
+                onPressed: () {playSound(6); },
+                child: const Text(''),
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {playSound(7); },
+                child: const Text(''),
+              ),
+            ],
           ),
         ),
       ),
